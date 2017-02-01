baf45d05f46b0831bf7686c9f4b95949364193d2
unknown <margot.valencia@TXBOMVALENCIA02.bo.truextend.com>
2017-02-01 15:04:56 -0400

    Added TCs for Login feature.

--- test_specs/features/step_definitions/Login_steps.rb
+++ test_specs/features/step_definitions/Login_steps.rb
@@ -4,6 +4,51 @@ Given(/^I go to the "([^"]*)" site$/) do |site|
   @loginpage = LoginPage.new(".//button[text()='Sign In']")
 end
 
-When(/^I login with "([^"]*)" username and "([^"]*)" password$/) do |user, password|
-  @loginpage.loginToPage(user,password)
+When(/^I login with "([^"]*)" (incorrect |)username and "([^"]*)" (incorrect |)password$/) do |username, incorrect_username, password, incorrect_password|
+  successful_login = !(incorrect_username.include?('incorrect') or incorrect_password.include?('incorrect'))
+  @loginpage.login(username, password, successful_login)
+end
+
+Given(/^I login to "([^"]*)" community with "([^"]*)" user$/) do |site, user|
+  @driverManager.loadUrl(@communityUtil.getCommunityUrl(site))
+  @siteutil.setCurrentSite(site)
+  @loginpage = LoginPage.new(".//button[text()='Sign In']")
+  @loginpage.loginWithReferences(user)
+end
+
+When(/^I make a Forgot Username request with "([^"]*)" user's email$/) do |user|
+  @loginpage.makeForgotUsernameRequest(user)
+end
+
+When(/^I make a Forgot Password request with "([^"]*)" user$/) do |user|
+  @loginpage.makeForgotPasswordRequest(user)
+end
+
+And(/^I verify "([^"]*)" text is displayed on the '(Forgot Username|Forgot Password)' page$/) do |text, _|
+  if text.include?('Sent to')
+    user_email = @userUtil.getUser(text[/user\((.*)\)/,1],'email')
+    final_text = text.gsub(text[/(\[.*\])....@/,1], user_email[0,1])
+    final_text = final_text.gsub(final_text[/@(\[.*\])/,1], user_email[/@(.*)/,1])
+  end
+  @loginpage.verifyForgotUsernamePwdText(final_text).should eq(true)
+end
+
+And(/^I verify that 'Return to (login|sign in)' link is displayed in '(Forgot Username|Forgot Password)' page$/) do |_,_|
+  @loginpage.verifyReturnToSignInLink.should eq(true)
+end
+
+And(/^I verify "([^"]*)" user received an email with "([^"]*)" subject$/) do |user, subject|
+  emailacc = @userUtil.getUser(user,'email')
+  emailpsw = @userUtil.getUser(user,'emailpsw')
+  match = emailacc[/\+(.*?)@/m]
+  emailacc = emailacc.gsub(match,'@') unless match.nil? #retrieving mail w/o alias
+  emailmanager = EmailManager.new
+
+  case subject
+    when 'Username Request','Reset Password Request' then
+      @inboxemails = emailmanager.getNoEmailsByContent(10, emailacc, emailpsw, subject, @userUtil.getUser(user,'username'))
+    else
+      fail(ArgumentError.new("'#{subject}' is not listed!"))
+  end
+  fail(ArgumentError.new("No '#{subject}' email was received!")) unless @inboxemails >= 1
 end
