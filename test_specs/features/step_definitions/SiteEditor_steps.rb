bde9a6b0d65f33aa749accc993abdfa0c8d3cd07
TRUEXTEND\walter.ramirez <walter.ramirez@mindjet.com>
2017-02-13 17:58:09 -0400

    no message

--- test_specs/features/step_definitions/SiteEditor_steps.rb
+++ test_specs/features/step_definitions/SiteEditor_steps.rb
@@ -50,6 +50,18 @@ And(/^I expand "([^"]*)" dropdown of "([^"]*)" column on Site Editor page$/) do 
   # TODO - 2/10/2017 - WR - Complete this step on *Create test steps ENG-10487* task
 end
 
+And(/^I expand the first "([^"]*)" dropdown$/) do |dropdown|
+  # TODO - 2/10/2017 - WR - Complete this step on *Create test steps ENG-10487* task
+end
+
 Then(/^I verify that '([^"]*)' widget (is|is not) displayed$/) do |widget, display_option|
   # TODO - 2/10/2017 - WR - Complete this step on *Create test steps ENG-10487* task
+end
+
+And(/^I click on the "([^"]*)" link$/) do |link|
+  # TODO - 2/13/2017 - WR - Complete this step on *Create test steps ENG-10487* task
+end
+
+And(/^I select "([^"]*)" tab$/) do |tab|
+  # TODO - 2/13/2017 - WR - Complete this step on *Create test steps ENG-10487* task
 end
