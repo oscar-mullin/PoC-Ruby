0b324597017dc77da3f663f4a291a2331419a9a1
Ferdinando Altamirano <ferdinando.altamirano@spigit.com>
2017-02-14 17:14:07 -0400

    MV changes

--- test_specs/features/step_definitions/SiteEditor_steps.rb
+++ test_specs/features/step_definitions/SiteEditor_steps.rb
@@ -1,56 +1,8 @@
-
 And(/^I expand "([^"]*)" dropdown of "([^"]*)" column on Site Editor page$/) do |dropdown, column|
-  #TODO
-end
-When(/^I click on 'Theme Editor' option on 'Administration' page$/) do
-  #TODO
+  # TODO - 2/10/2017 - WR - Complete this step on *Create test steps ENG-10487* task
 end
 
-Then(/^I verify the 'Save' button is (enabled|disabled) on 'Theme Editor' page$/) do |enabled|
-  #TODO
-end
-
-When(/^I click on the 'Brand Color' circle$/) do
-  #TODO
-end
-
-Then(/^I verify the color picker is displayed$/) do
-  #TODO
-end
-
-When(/^I fill in the Hex color field with "([^"]*)" code$/) do |code|
-  #TODO
-end
-
-Then(/^I verify Hex color field value is "([^"]*)"$/) do |code|
-  #TODO
-end
-
-When(/^I click on the 'Save' button on 'Theme Editor' page$/) do
-  #TODO
-end
-
-And(/^I verify "([^"]*)" popup message is displayed on 'Theme Editor' page$/) do |message|
-  #TODO
-end
-
-Then(/^I verify the idea's rating has "([^"]*)" color$/) do |color|
-  #TODO
-end
-
-Then(/^I verify the navigation bar has "([^"]*)" color$/) do |color|
-  #TODO
-end
-
-And(/^I verify the 'Ideas' tab has "([^"]*)" color$/) do |color|
-  #TODO
-end
-
-Then(/^I verify 'Publish' button has "([^"]*)" color$/) do |color|
-  #TODO
-end
-
-And(/^I expand the first 'Select Widget' dropdown$/) do
+And(/^I expand the first "([^"]*)" dropdown$/) do |dropdown|
   # TODO - 2/10/2017 - WR - Complete this step on *Create test steps ENG-10487* task
 end
 
@@ -64,4 +16,9 @@ end
 
 And(/^I select 'Content' tab$/) do |tab|
   # TODO - 2/13/2017 - WR - Complete this step on *Create test steps ENG-10487* task
+end
+
+
+And(/^I expand the first 'Select Widget' dropdown$/) do
+  pending
 end
